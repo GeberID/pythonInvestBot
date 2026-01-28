0e91eda43c5f3d22e7b9b445ecde663468a5994f
Alexander <aleksanderfranorge@gmail.com>
2026-01-28 21:48:43 +0300

    all positions

--- invest_bot/core/invest.py
+++ invest_bot/core/invest.py
@@ -1,14 +1,26 @@
+from enum import Enum
+
 from t_tech.invest import PortfolioResponse, MoneyValue
 
 def get_money(money_value:MoneyValue) -> float:
     return float(f"{money_value.units}.{money_value.nano}")
 
+class InstrumentType(Enum):
+    SHARE = "share"
+    BOND = "bond"
+    ETF = "etf"
+    CURRENCY = "currency"
+
 class Portfolio:
     def __init__(self, portfolio: PortfolioResponse):
+        self._free_money,self._all_currency_positions = self._get_currencies(self, portfolio)
+        self._all_shares_positions = self._get_all_positions(portfolio, InstrumentType.SHARE)
+        self._all_bonds_positions = self._get_all_positions(portfolio, InstrumentType.BOND)
+        self._all_etfs_positions = self._get_all_positions(portfolio, InstrumentType.ETF)
         self._bonds = get_money(portfolio.total_amount_bonds)
         self._etfs = get_money(portfolio.total_amount_etf)
         self._shares = get_money(portfolio.total_amount_shares)
-        self._currencies = get_money(portfolio.total_amount_currencies)
+        self._currencies = get_money(portfolio.total_amount_currencies) - self._free_money
         self._all_portfolio = self._bonds + self._etfs + self._currencies + self._shares
 
     def print_common_portfolio_info(self) -> str:
@@ -16,7 +28,8 @@ class Portfolio:
                 f"Акции - {self._shares}\n"
                 f"Облигации - {self._bonds}\n"
                 f"Фонды - {self._etfs}\n"
-                f"Валюта - {self._currencies}\n"
+                f"Валюта и драгметалы - {self._currencies}\n"
+                f"Свободной валюты - {self._free_money}\n"
                 f"Всего - {self._all_portfolio}")
 
     def print_portfolio_structure(self) -> str:
@@ -24,7 +37,18 @@ class Portfolio:
                 f"Акции - {self._get_percentage_from_element(self._shares)}\n"
                 f"Облигации - {self._get_percentage_from_element(self._bonds)}\n"
                 f"Фонды - {self._get_percentage_from_element(self._etfs)}\n"
-                f"Валюта - {self._get_percentage_from_element(self._currencies)}\n")
+                f"Валюта и драгметалы - {self._get_percentage_from_element(self._currencies)}\n"
+                f"Свободной валюты - {self._get_percentage_from_element(self._free_money)}\n")
 
     def _get_percentage_from_element(self, money_value:MoneyValue) -> float:
         return (money_value/ self._all_portfolio) * 100.00
+
+    @staticmethod
+    def _get_all_positions(portfolio: PortfolioResponse, instrument_type:InstrumentType) ->list[MoneyValue]:
+        return [element for element in portfolio.positions if element.instrument_type == instrument_type.value]
+
+    @staticmethod
+    def _get_currencies(self, portfolio: PortfolioResponse, ):
+        all_currencies_positions = self._get_all_positions(portfolio, InstrumentType.CURRENCY)
+        free_money = [element for element in all_currencies_positions if element.ticker == "RUB000UTSTOM"][0].quantity
+        return get_money(free_money),all_currencies_positions
--- invest_bot/main.py
+++ invest_bot/main.py
@@ -1,10 +1,10 @@
-import api.tinkoff_api as tinkoff_api
+import api.tinkoff_api as api
 from invest_bot.core.invest import Portfolio
 
 
 def main():
-    account_id = tinkoff_api.get_accounts().accounts[0].id
-    portfolio = Portfolio(tinkoff_api.get_portfolio(account_id))
+    account_id = api.get_accounts().accounts[0].id
+    portfolio = Portfolio(api.get_portfolio(account_id))
     print(portfolio.print_common_portfolio_info())
     print(portfolio.print_portfolio_structure())
 
