b19f794aacd2efbce17ae40a1abf34f1e740b88f
a.guslyannikov <sanya19976@gmail.com>
2026-02-23 23:05:36 +0300

    small refactoring

--- invest_bot/core/portfolio.py
+++ invest_bot/core/portfolio.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 from t_tech.invest import PortfolioResponse, PortfolioPosition
 
+from invest_bot.api import tinkoff_api as api
 from invest_bot.core.logs import log
 from invest_bot.core.money_utilities import get_money, get_percentage_from_element
 
@@ -16,26 +17,17 @@ class InstrumentType(Enum):
     CURRENCY = "currency"
 
 class Portfolio:
+    account_id: str
     _portfolio: PortfolioResponse
-    freeMoney: Decimal
+    free_money: Decimal
     positions: dict[InstrumentType, list[PortfolioPosition]]
-    positionsAmt: dict[InstrumentType,Decimal]
+    positions_amt: dict[InstrumentType,Decimal]
 
-    def __init__(self, portfolio: PortfolioResponse):
-        self._portfolio = portfolio
-        self.positions = {
-            InstrumentType.SHARE:self._get_all_positions(InstrumentType.SHARE),
-            InstrumentType.BOND:self._get_all_positions(InstrumentType.BOND),
-            InstrumentType.ETF:self._get_all_positions(InstrumentType.ETF),
-            InstrumentType.CURRENCY:self._get_all_currencies_positions(),
-        }
-        self.positionsAmt = {
-            InstrumentType.SHARE: get_money(portfolio.total_amount_shares),
-            InstrumentType.BOND: get_money(portfolio.total_amount_bonds),
-            InstrumentType.ETF: get_money(portfolio.total_amount_etf),
-            InstrumentType.CURRENCY: get_money(portfolio.total_amount_currencies),
-        }
-        self.freeMoney = self._update_free_money()
+    def __init__(self,account_id:str):
+        self.update_portfolio(account_id)
+        self.update_positions()
+        self.update_positions_amt()
+        self.update_free_money()
 
     def __repr__(self):
         return f"{self.__class__.__name__}"
@@ -44,11 +36,11 @@ class Portfolio:
     def print_common_info_str(self) -> str:
         return (
             f"Портфолио:\n"
-            f"Акции - {self.positionsAmt.get(InstrumentType.SHARE):,.2f} ₽\n"
-            f"Облигации - {self.positionsAmt.get(InstrumentType.BOND):,.2f} ₽\n"
-            f"Фонды - {self.positionsAmt.get(InstrumentType.ETF):,.2f} ₽\n"
-            f"Валюта и драгметалы - {self.positionsAmt.get(InstrumentType.CURRENCY) - self.freeMoney:,.2f} ₽\n"
-            f"Свободной валюты - {self.freeMoney:,.2f} ₽\n"
+            f"Акции - {self.positions_amt.get(InstrumentType.SHARE):,.2f} ₽\n"
+            f"Облигации - {self.positions_amt.get(InstrumentType.BOND):,.2f} ₽\n"
+            f"Фонды - {self.positions_amt.get(InstrumentType.ETF):,.2f} ₽\n"
+            f"Валюта и драгметалы - {self.positions_amt.get(InstrumentType.CURRENCY) - self.free_money:,.2f} ₽\n"
+            f"Свободной валюты - {self.free_money:,.2f} ₽\n"
             f"------------------------------\n"
             f"Всего - {self._all_portfolio_money():,.2f} ₽"
         )
@@ -58,30 +50,36 @@ class Portfolio:
         all_portfolio = self._all_portfolio_money()
         return (
             f"Процентное соотношение:\n"
-            f"Акции - {get_percentage_from_element(self.positionsAmt.get(InstrumentType.SHARE), all_portfolio)}\n"
-            f"Облигации - {get_percentage_from_element(self.positionsAmt.get(InstrumentType.BOND), all_portfolio)}\n"
-            f"Фонды - {get_percentage_from_element(self.positionsAmt.get(InstrumentType.ETF), all_portfolio)}\n"
-            f"Валюта и драгметалы - {get_percentage_from_element(self.positionsAmt.get(InstrumentType.CURRENCY), all_portfolio)}\n"
+            f"Акции - {get_percentage_from_element(self.positions_amt.get(InstrumentType.SHARE), all_portfolio)}\n"
+            f"Облигации - {get_percentage_from_element(self.positions_amt.get(InstrumentType.BOND), all_portfolio)}\n"
+            f"Фонды - {get_percentage_from_element(self.positions_amt.get(InstrumentType.ETF), all_portfolio)}\n"
+            f"Валюта и драгметалы - {get_percentage_from_element(self.positions_amt.get(InstrumentType.CURRENCY), all_portfolio)}\n"
         )
 
     @log
     def print_all_shares(self):
         shares_data = [
-            (position.ticker, get_money(position.current_price) * get_money(position.quantity))
+            (position.ticker,
+             get_money(position.current_price) * get_money(position.quantity),
+             position.daily_yield)
             for position in self.positions.get(InstrumentType.SHARE)
         ]
         shares_data.sort(key=lambda x: x[1], reverse=True)
-        lines = [f"<code>{t:<6}</code> — {amt:,.2f} ₽" for t, amt in shares_data]
+        lines = [f"<code>{t:<6}</code> — {amt:,.2f} — {daily_yield:,.2f} ₽" for t, amt, daily_yield in shares_data]
         return "Акции\n" + "\n".join(lines)
 
     @log
     def print_all_bonds(self):
         bonds_data = [
-            (position.ticker, get_money(position.current_price) * get_money(position.quantity), position.current_nkd)
+            (position.ticker,
+             get_money(position.current_price) * get_money(position.quantity),
+             position.current_nkd,
+             position.daily_yield)
             for position in self.positions.get(InstrumentType.BOND)
         ]
         bonds_data.sort(key=lambda x: x[1], reverse=True)
-        lines = [f"<code>{t:<6}</code> — {amt:,.2f} ₽ - Нкд: {get_money(nkd):,.2f}" for t, amt, nkd in bonds_data]
+        lines = [f"<code>{t:<6}</code> — {amt:,.2f} ₽ - Нкд: {get_money(nkd):,.2f} — {daily_yield:,.2f}"
+                 for t, amt, nkd, daily_yield in bonds_data]
         return "Облигации\n" + "\n".join(lines)
 
     @log
@@ -94,18 +92,36 @@ class Portfolio:
         return Decimal(-1)
 
     @log
-    def _update_free_money(self) -> Decimal:
-        free_money = [element for element in self.positions.get(InstrumentType.CURRENCY)
+    def update_free_money(self) -> None:
+        temp_free_money = [element for element in self.positions.get(InstrumentType.CURRENCY)
                       if element.ticker == RUB_TICKER][0].quantity
-        return get_money(free_money)
+        self.free_money = get_money(temp_free_money)
 
     @log
-    def _get_all_positions(self, instrument_type: InstrumentType) -> list[PortfolioPosition]:
+    def update_positions(self) -> None:
+        self.positions = {
+            InstrumentType.SHARE: self._get_positions(InstrumentType.SHARE),
+            InstrumentType.BOND: self._get_positions(InstrumentType.BOND),
+            InstrumentType.ETF: self._get_positions(InstrumentType.ETF),
+            InstrumentType.CURRENCY: self._get_positions(InstrumentType.CURRENCY),
+        }
+
+    @log
+    def update_positions_amt(self) -> None:
+        self.positions_amt = {
+            InstrumentType.SHARE: get_money(self._portfolio.total_amount_shares),
+            InstrumentType.BOND: get_money(self._portfolio.total_amount_bonds),
+            InstrumentType.ETF: get_money(self._portfolio.total_amount_etf),
+            InstrumentType.CURRENCY: get_money(self._portfolio.total_amount_currencies),
+        }
+
+    @log
+    async def update_portfolio(self) -> None:
+        self._portfolio = api.get_portfolio(self.account_id)
+
+    @log
+    def _get_positions(self, instrument_type: InstrumentType) -> list[PortfolioPosition]:
         return [p for p in self._portfolio.positions if p.instrument_type == instrument_type.value]
-
-    @log
-    def _get_all_currencies_positions(self) -> list[PortfolioPosition]:
-        return self._get_all_positions(InstrumentType.CURRENCY)
 
     @log
     def _all_portfolio_money(self) -> Decimal:
--- invest_bot/main.py
+++ invest_bot/main.py
@@ -23,7 +23,7 @@ async def command_start_handler(message: Message) -> None:
 
 @dp.message(Command("portfolio"))
 async def command_portfolio_handler(message: Message, account_id: str) -> None:
-    portfolio = Portfolio(await api.get_portfolio(account_id))
+    portfolio = Portfolio(account_id)
     await message.answer(f"{portfolio.print_common_info_str()}\n\n" + f"{portfolio.print_persent_structure_str()}")
     await message.answer(f"{ portfolio.print_all_shares()}")
     await message.answer(f"{ portfolio.print_all_bonds()}")
